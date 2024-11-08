b886c0b8ef11516c6a9ec674462e03a773645569
Abhishek <avishek.npt@gmail.com>
2024-11-08 19:10:05 +0530

    modified cors options

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const corsOptions = {
-  origin: ["https://book-says-up.vercel.app", "http://localhost:3000"],
+  origin: ["https://book-says-q8m54xb7o-avigithubbs-projects.vercel.app", "http://localhost:3000"],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
